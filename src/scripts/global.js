60530d4f9c9a36ce031afb3d15c8badb5886211d
Raick Miranda <mirandaraick@gmail.com>
2022-07-07 13:36:45 -0300

    Delete src/scripts directory

--- src/scripts/global.js
+++
@@ -1,8 +0,0 @@
-// Verificar se tem uma sessao valida, se tiver, esconder a parte de login e cadastro.
-
-console.log("chamado!");
-
-if (localStorage.getItem("@name")) {
-  const AuthContainer = document.getElementsByClassName("logins-container")[0];
-  AuthContainer.remove();
-}
